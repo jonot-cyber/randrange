e3fb4627239c5ce5cb914f86e4ba5a1822f2e945
Joseph Tessitore <joetgamerman3@gmail.com>
2022-11-26 10:01:52 -0500

    Add flags

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"flag"
+)
 
+func main() {
+	mathMode := flag.String("m", "", "Example: [0, 20)")
+	standardMode := flag.String("s", "", "Example: 0-21")
+	flag.Parse()
 }
